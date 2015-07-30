291473e7d24a5c8b98da4ab3ee8c51bdbf2bd34d
Samantha Rose Cruz <samrosecruz@gmail.com>
2015-07-30 16:59:03 +0800

    adds animation shtz

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,40 @@
 $(function() {
   var loaderSvg = Snap('#svg-loader');
+
   var loadingCircle = loaderSvg.select('#pc-loading-circle-filled'),
-    circumf = Math.PI*(loadingCircle.attr('r')*2);
-   
-  loadingCircle.attr({
-    'stroke-dasharray': circumf+' '+circumf,
-    'stroke-dashoffset': circumf,
-  });
+      loadingSvg = loaderSvg.select('#pc-loading-circle'),
+      playBtn = loaderSvg.select('#pc-loading-play'),
+      pauseBtn = loaderSvg.select('#pc-loading-pause'),
+      circumf = Math.PI*(loadingCircle.attr('r')*2);
 
-  //var playBtn = animatingSvg.select('#cd-play-btn').
-    //globalAnimation;
-   
-  //playBtn.click(function(){	
-    //var strokeOffset = loadingCircle.attr('stroke-dashoffset').replace('px', '');
-    ////animate strokeOffeset desn't work with circle element - we need to use Snap.animate() rather than loadingCircle.animate()
-    //globalAnimation = Snap.animate(strokeOffset, '0', function( value ){ 
-      //loadingCircle.attr({ 'stroke-dashoffset': value })
-      //}, 1500, mina.easein
-    //);
-  //});
+	//this variable will be used to store the loadingCircle animation object
+	var globalAnimation;
 
+	initLoading();	
+
+  $('#pc-loading-pause').hide();;
+	
+  playBtn.click(function(){
+		var strokeOffset = loadingCircle.attr('stroke-dashoffset').replace('px', '');
+    $('#pc-loading-play').hide();
+    $('#pc-loading-pause').show();;
+
+		//animate strokeOffeset desn't work with circle element - we need to use Snap.animate() rather than loadingCircle.animate()
+		globalAnimation = Snap.animate(strokeOffset, '0', function( value ){ 
+			loadingCircle.attr({ 'stroke-dashoffset': value })
+			}, (strokeOffset/circumf)*1500, mina.easein, function(){
+				$('#pc-loading-circle-filled').fadeOut();
+        $('#pc-loading-play').show();
+        $('#pc-loading-pause').hide();;
+			}
+		);
+	});
+
+	function initLoading() {
+		loadingCircle.attr({
+			'stroke-dasharray': circumf+' '+circumf,
+			'stroke-dashoffset': circumf,
+		});
+	}
 });
 
