f287d6f7c8ae21c38633cfda0dbccc73a870666b
Samantha Rose Cruz <samrosecruz@gmail.com>
2015-07-31 18:48:00 +0800

    adds another arrow

--- assets/script.js
+++ assets/script.js
@@ -13,13 +13,33 @@ $(function() {
 
 $(function() {
   var arrowHorizontalSvg = Snap('#arrow-horizontal');
-  var arrowHead = arrowHorizontalSvg.select('#arrow-head');
+  var arrowHead = arrowHorizontalSvg.select('#arrow-head-h');
 
   arrowHorizontalSvg.click(function() {
-    arrowHead.animate({d:"M101.4824219,0 L157,82.0410156 L101.4824219,164.082031"}, 300, mina.easein);
+    if (arrowHorizontalSvg.hasClass('animated')) {
+      arrowHead.animate({d:"M66.4824219,0 L0,82.0410156 L66.4824219,164.082031"}, 300, mina.easein);
+      arrowHorizontalSvg.removeClass('animated');
+    } else {
+      arrowHead.animate({d:"M101.4824219,0 L157,82.0410156 L101.4824219,164.082031"}, 300, mina.easein);
+      arrowHorizontalSvg.addClass('animated');
+    }
   });
 }); 
 
+$(function() {
+  var arrowVerticalSvg = Snap('#arrow-vertical');
+  var arrowHead = arrowVerticalSvg.select('#arrow-head-v');
+
+  arrowVerticalSvg.click(function() {
+    if (arrowVerticalSvg.hasClass('animated')) {
+      arrowHead.animate({d:"M115.482422,-48 L49,34.0410156 L115.482422,116.082031"}, 300, mina.easein);
+      arrowVerticalSvg.removeClass('animated');
+    } else {
+      arrowHead.animate({d:"M130.482422,-48 L196,34.0410156 L130.482422,116.082031"}, 300, mina.easein);
+      arrowVerticalSvg.addClass('animated');
+    }
+  });
+}); 
 /////// Loading Animation
 $(function() {
   var loaderSvg = Snap('#svg-loader');
